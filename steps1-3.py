eb0c347026983d91ac3857c62c6dee02ecfc903e
Ryan Healy <bnz9cu@virginia.edu>
2026-02-04 17:10:17 +0000

    further updated file

--- steps1-3.py
+++ steps1-3.py
@@ -48,11 +48,63 @@ collegedf.dtypes
 """
 Fix factor levels: Check that categorical variables don't have too many groups.
 """
-collegedf.city.value_counts()[0:10]
+collegedf.chronname.value_counts()[0:10]
+# Not worth altering because there are far too many schools to categorize.
+#%%
+collegedf.city.value_counts()[20:40]
+# Too many cities with high numbers to categorize.
 
 #%%
-# Fixing city category to only be top
+collegedf.state.value_counts()
+# Going to categorize into Midwest, Northeast, Pacific, Southeast, and West.
 
+#%%
+midwest = ['Texas','Oklahoma','Kansas','Nebraska','South Dakota','North Dakota',
+           'Minnesota','Wisconsin','Iowa','Missouri','Arkansas','Louisiana',
+           'Illinois','Indiana','Ohio','Michigan']
+
+northeast = ['Pennsylvania','New York','New Jersey','Connecticut','Rhode Island',
+             'Massachusetts','Vermont','New Hampshire','Maine']
+
+pacific = ['California','Oregon','Washington','Alaska','Hawaii']
+
+southeast = ['Kentucky','Tennessee','Virginia','West Virginia','Maryland',
+             'Delaware','North Carolina','South Carolina','Mississippi',
+             'Alabama','Georgia','Florida']
+
+west = ['Montana','Idaho','Wyoming','Nevada','Utah','Colorado','Arizona','New Mexico']
+
+#%%
+# Lambda function explanation:
+# lambda x: "Region_Name" if x in region_variable else x
+# If the state falls into that region, then it will be renamed to said region
+# This is done by checking if it is in the list for that region
+# If it is not, then it will simply return x, or its original unaltered nanme
+collegedf.state = collegedf.state.apply(lambda x: "Midwest" if x in midwest else x)
+collegedf.state = collegedf.state.apply(lambda x: "Northeast" if x in northeast else x)
+collegedf.state = collegedf.state.apply(lambda x: "Pacific" if x in pacific else x)
+collegedf.state = collegedf.state.apply(lambda x: "Southeast" if x in southeast else x)
+collegedf.state = collegedf.state.apply(lambda x: "West" if x in west else x)
+
+#%%
+# Check that lambda functions worked as intended.
+collegedf.state
+
+#%%
+collegedf.level.value_counts()
+# Already organized into two categories.
+
+#%%
+collegedf.control.value_counts()
+# Already well organized into three categories.
+
+#%%
+collegedf.basic.value_counts()
+# Maybe categorize into specific programs based on Associates, Theological, Masters?
+
+#%%
+collegedf.site.value_counts()
+# These are websites and don't need to be categorized.
 #%%
 # Prevalence is calculating the percentage of targets that pass (0 or 1) (ballpark 10-20%)
 # Usually care about modeling something that occurs a small percentage of the time.
